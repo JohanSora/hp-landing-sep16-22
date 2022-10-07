abd8d2dbc2903e4136188adcd966b83d99d97aab
JohanSora <johan.sora@mymarketlogic.com>
2022-10-07 14:10:02 -0500

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,6 @@ function App() {
         headers: {
           "xh-client-id": "6672ff3aaef044ff9c11fc34651e6108",
           "xh-client-secret": "Ae63abFe0bC34bA7bF6e4A2Af8d61907",
-          "Content-Type": "application/json; charset=UTF-8",
-          "Strict-Transport-Security": "max-age=31536000; includeSubdomains;",
-          "X-Content-Type-Options": "nosniff",
-          "X-Frame-Options": "SAMEORIGIN",
-          "X-XSS-Protection": "1; mode=block",
-          "X-Cache": "CONFIG_NOCACHE",
-          "X-Azure-Ref":
-            "0fXZAYwAAAACrYijuMXIOSZKylWtiQD0LQk4xQUEyMDUxMDIxMDI5ADllZWFiNjdiLThhMTUtNDhiMy1iN2Q2LTI0ZDQzNzBkNzJlNA==",
         },
       }
     )
